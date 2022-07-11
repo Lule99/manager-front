a62ba08e268cbd0f3df6e52b198a3ec335f86448
blagojevic-nj <blagojevic.njegos1999@gmail.com>
2022-07-11 15:58:46 +0200

    Request approving/rejecting implemented

--- src/services/ZahtevService.js
+++ src/services/ZahtevService.js
@@ -4,3 +4,18 @@ import { SPRING_APP_URL } from "../helpers/constants";
 export const noviZahtevi = () => {
   return axios.get(`${SPRING_APP_URL}/api/zahtev/novi`);
 };
+
+export const dokumentaZaZahtev = (id) => {
+  return axios.get(`${SPRING_APP_URL}/api/zahtev/dokumenta-za-zahtev/${id}`);
+};
+
+export const prihvatiZahtev = (id) => {
+  return axios.post(`${SPRING_APP_URL}/api/zahtev/accept/${id}`);
+};
+
+export const odbijZahtev = (id, razlog) => {
+  return axios.post(
+    `${SPRING_APP_URL}/api/zahtev/reject/${id}/${razlog}`,
+    null
+  );
+};
